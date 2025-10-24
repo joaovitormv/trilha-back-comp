324324b11d4e79adb628d22da28b4eb5fb526953
joaovitormv <126258486+joaovitormv@users.noreply.github.com>
2025-10-24 00:37:25 +0000

    iniciando o projeto final

---
+++ src/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express')
+const app = express();
+const routes = require('./routes')
+
+app.use(express.json());
+app.use(routes);
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Server is on in port ${PORT}`);
+})
